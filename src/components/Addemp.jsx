a127b0ae29ba4540bbfbd452c6f85c237cb95f02
AbhinandNR <AbhinandNR>
2025-07-14 22:21:53 +0530

    Created files

---
+++ src/components/Addemp.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Addemp = () => {
+  return (
+    <div>
+
+        <div className="conatiner">
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                    <div className="row g-3">
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-label" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-label" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-label" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-label" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-label" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-label" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Addemp

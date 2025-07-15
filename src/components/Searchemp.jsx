8bdb4370bbd410e3d2354cdd27a02f5b031ec168
AbhinandNR <AbhinandNR>
2025-07-15 22:49:24 +0530

    tested

--- src/components/Searchemp.jsx
+++ src/components/Searchemp.jsx
@@ -9,9 +9,10 @@ const Searchemp = () => {
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
 
-                        <div className="row">
+                        <div className="row g-3">
                             <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="Emp Name" className="form-label">Employee Name</label>
+                                <input type="text" className="form-control" />
                             </div>
                             <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <button className="btn btn-success">Search</button>
--- src/components/Viewemp.jsx
+++ src/components/Viewemp.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Empnav from './Empnav'
 
 const Viewemp = () => {
 
@@ -22,6 +23,7 @@ const Viewemp = () => {
     useEffect(() => { fetchdatafrom() }, [])
     return (
         <div>
+            <Empnav/>
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
